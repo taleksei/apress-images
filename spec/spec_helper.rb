b5247d3bc8f49d2e9def36248eb69616f7b4bd9c
TamarinEA <silfar@inbox.ru>
2016-03-14 12:09:10 +0500

    fix: change image status when save with delay processing
    https://jira.railsc.ru/browse/BPC-7717

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,8 @@ require 'bundler/setup'
 require 'simplecov'
 SimpleCov.start 'rails' do
   minimum_coverage 95
+  add_filter 'lib/apress/images/engine.rb'
+  add_filter 'lib/apress/images/version.rb'
 end
 
 require 'apress/images'
